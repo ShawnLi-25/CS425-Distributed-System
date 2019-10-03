f8bd692f56a9abb603cfafc6a5695b4748f3b0aa
Liu <taipeng2@fa19-cs425-g73-02.cs.illinois.edu>
2019-10-03 16:11:15 -0500

    node.go

--- src/MP2/src/Node/Node.go
+++ src/MP2/src/Node/Node.go
@@ -7,18 +7,32 @@ import (
 )
 
 type Node struct {
-	node.Sender
-	node.Listener
-	node.Introducer
-	node.Updater
+	Sender     node.Sender
+	Listener   node.Listener
+	Introducer node.Introducer
+	Updater    node.Updater
 }
+
+func CreateIntroducerNode() Node{
+
+}
+
+func CreateNonIntroducerNode() Node{
+
+}
+
 
 
 //Called from main.go when the command is "JOIN\n"
 //Create new node and run the node until LEAVE or crash
 func RunNode(isIntroducer bool) {
+	var node Node
 	if(!isIntroducer){
+		node = CreateNonIntroducerNode()
+		RunNonIntroducerNode(node)
 	} else {
+		node = CreateIntroducerNode()
+		RunIntroducerNode(node)
 	}
 }
 
--- src/MP2/src/main.go
+++ src/MP2/src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//node "MP2/src/node"
 	"fmt"
 	"./Helper"
 	"./Node"
@@ -14,22 +13,8 @@ import (
 const (
 	IntroducerAddress = "fa19-cs425-g73-01.cs.illinois.edu"
 )
-/*
-type Node struct {
-	node.Sender
-	node.Listener
-	node.Introducer
-	node.Updater
-}
-*/
-func main() {
-	// isJoinPtr := flag.Bool("join", false, "join the group")
-	// isLeavePtr := flag.Bool("leave", false, "voluntarily leave the group")
-	// showListPtr := flag.Bool("membership", false, "show the current membership list")
-	// showIDPtr := flag.Bool("ID", false, "show self's ID")
 
-	// flag.Parse()
-	
+func main() {
 	isIntroducer := helper.IsIntroducer()
 
 	reader := bufio.NewReader(os.Stdin)
@@ -53,28 +38,15 @@ func main() {
 				continue
 		}
 	}
-	/*
-	fmt.Println("Start running server...")
-
+	/**
 	logFile, fileErr := os.OpenFile("MP2.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if fileErr != nil {
 		log.Println(fileErr)
 	}
 	defer logFile.Close()
-
 	var updateChan = make(chan node.UpdateQuery)
 	go node.UpdateMembershipList(updateChan)
 	go node.ListenMsg()
-	// switch {
-	// case *isJoinPtr:
-
-	// }
-	*/
-}
-
-func NodeBehavior() {
-	/**
-	Check if this server is introducer (hard core to VM01)
-	Execute different logic
 	**/
 }
+
