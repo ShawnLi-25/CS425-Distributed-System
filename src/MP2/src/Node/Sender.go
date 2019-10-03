0c5c0ef2530c7e1e7db1ac6bdca82f38a05d1317
Xiang Li <lidiang2507@gmail.com>
2019-10-03 15:18:45 -0500

    Sender

--- src/MP2/src/Node/Sender.go
+++ src/MP2/src/Node/Sender.go
@@ -13,7 +13,7 @@ import (
 type Sender struct{}
 
 func (s *Sender) SendHeartbeat(monitorAddress string, monitorID string, localID string) {
-	heartBeatMsg := msg.NewMessage("HeartBeat", localID, []string{})
+	heartBeatMsg := msg.NewMessage(msg.HeartbeatMsg, localID, []string{})
 	heartBeatPkg := msg.MsgToJSON(heartBeatMsg)
 
 	for {
@@ -39,14 +39,14 @@ func (s *Sender) SendHeartbeat(monitorAddress string, monitorID string, localID 
 	}
 }
 
-func CreateID() string {
-	cmd := exec.Command("hostname")
-	hName, _ := cmd.Output()
-	hostName := string(hName)
-	localTime := time.Now()
-	// fmt.Println(localTime.Format(time.RFC3339))
-	return hostName + ":" + msg.ConnPort + "+" + localTime.Format("20060102150405")
-}
+func (s *Sender) SendLeaveMsg(monitorAddress string, monitorID string, localID string) {
+	leaveMsg := msg.NewMessage(msg.LeaveMsg, localID, []string{})
+	leavePkg := msg.MsgToJSON(leaveMsg)
+
+	udpAddr, err := net.ResolveUDPAddr(msg.ConnType, monitorAddress)
+		if err != nil {
+			log.Println(err.Error())
+			os.Exit(1)
 		}
 		conn, err := net.DialUDP(msg.ConnType, nil, udpAddr)
 		if err != nil {
@@ -63,10 +63,28 @@ func CreateID() string {
 		log.Print("===LeaveMsg Sent to: " + string(monitorID) + "\n" + "===Msg is" + string(msg))
 }
 
-func (s *Sender) SendJoinMsg(monitorAddress string, monitorID string, localID string) {
+func (s *Sender) SendJoinMsg(introducerAddress string, localID string) {
 	joinMsg := msg.NewMessage(msg.JoinMsg, localID, []string{})
 	joinPkg := msg.MsgToJSON(joinMsg)
 
+	udpAddr, err := net.ResolveUDPAddr(msg.ConnType, monitorAddress)
+		if err != nil {
+			log.Println(err.Error())
+			os.Exit(1)
+		}
+		conn, err := net.DialUDP(msg.ConnType, nil, udpAddr)
+		if err != nil {
+			log.Println(err.Error())
+			os.Exit(1)
+		}
+
+		msg, err := conn.Write(leavePkg)
+		if err != nil {
+			log.Println(err.Error())
+			os.Exit(1)
+		}
+
+		log.Print("===LeaveMsg Sent to: " + string(monitorID) + "\n" + "===Msg is" + string(msg))
 }
 
 func CreateID() string {
