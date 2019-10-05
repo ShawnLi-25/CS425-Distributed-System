2c39c00cc8ce3689ee4fc91fb3cfa6a4f8f2d008
Xiang Li <lidiang2507@gmail.com>
2019-10-05 12:11:53 -0500

    node/Listener.go

--- src/MP2/src/Node/Listener.go
+++ src/MP2/src/Node/Listener.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net"
-	msg "../Helper"
 	"time"
+
+	msg "../Helper"
 )
 
 // Listener is a type that implements the ListenMsg(), ListenJoinMsg() "method"
 type Listener struct {
 }
-
 
 func (l *Listener) RunMSGListener() {
 
@@ -27,21 +27,20 @@ func (l *Listener) RunMSGListener() {
 	}
 	fmt.Printf("Listener:MSGListener listen on port %s\n", msg.ConnPort)
 
-
 	for {
-		fmt.Println("Listener: Works!!")			
-		HandleListenMsg(ln)
+
 		select {
-			case <- KillRoutine:
+			case <-KillRoutine:
 				fmt.Println("Listener: Go Routine Closed")
 				ln.Close()
-				return 
-			// default:	
-				
-		}	
-			
+				return
+			default:
+				fmt.Println("Listener: Works!!")
+				HandleListenMsg(ln)
+		}
+
 	}
-	
+
 }
 
 func HandleListenMsg(conn *net.UDPConn) {
@@ -52,34 +51,33 @@ func HandleListenMsg(conn *net.UDPConn) {
 		log.Fatal(err)
 	}
 	receivedMsg := msg.JSONToMsg([]byte(string(msgBuf[:n])))
-	log.Printf("Listender: Recieve %s message from Node: %s, Addrs: %s", receivedMsg.MessageType,receivedMsg.NodeID, msgAddr)
+	log.Printf("Listender: Recieve %s message from Node: %s, Addrs: %s", receivedMsg.MessageType, receivedMsg.NodeID, msgAddr)
 
 	switch receivedMsg.MessageType {
-		case msg.FailMsg:
-			UpQryChan <- UpdateQuery{2, receivedMsg.NodeID}
-			retMemList := <-MemListChan
-			if len(retMemList) != 0 {
-				SendFailMsg(conn, receivedMsg.NodeID)
-			}
-		case msg.LeaveMsg:
-			UpQryChan <- UpdateQuery{2, receivedMsg.NodeID}
-			retMemList := <-MemListChan
-			if len(retMemList) != 0 {
-				SendFailMsg(conn, receivedMsg.NodeID)
-			}
-		case msg.IntroduceMsg:
-			fmt.Println("Listener: receive IntroduceMsg")
-			UpQryChan <- UpdateQuery{1, receivedMsg.Content[0]}
-			retMemList := <-MemListChan
-			if len(retMemList) != 0 {
-				SendIntroduceMsg(conn, receivedMsg.Content[0])
-			}
-		default:
-			fmt.Println("Listener:Can't recognize the msg")
+	case msg.FailMsg:
+		UpQryChan <- UpdateQuery{2, receivedMsg.NodeID}
+		retMemList := <-MemListChan
+		if len(retMemList) != 0 {
+			SendFailMsg(conn, receivedMsg.NodeID)
+		}
+	case msg.LeaveMsg:
+		UpQryChan <- UpdateQuery{2, receivedMsg.NodeID}
+		retMemList := <-MemListChan
+		if len(retMemList) != 0 {
+			SendFailMsg(conn, receivedMsg.NodeID)
+		}
+	case msg.IntroduceMsg:
+		fmt.Println("Listener: receive IntroduceMsg")
+		UpQryChan <- UpdateQuery{1, receivedMsg.Content[0]}
+		retMemList := <-MemListChan
+		if len(retMemList) != 0 {
+			SendIntroduceMsg(conn, receivedMsg.Content[0])
+		}
+	default:
+		fmt.Println("Listener:Can't recognize the msg")
 	}
-	return 
+	return
 }
-
 
 //Listen to Heartbeat and Check timeout
 func (l *Listener) RunHBListener() {
@@ -98,7 +96,7 @@ func (l *Listener) RunHBListener() {
 	defer ln.Close()
 	hbBuf := make([]byte, 128)
 	ln.SetReadDeadline(time.Now().Add(msg.TimeOut))
-	for{
+	for {
 		n, msgAddr, err := ln.ReadFromUDP(hbBuf)
 		if err != nil {
 			log.Fatal(err)
@@ -111,7 +109,7 @@ func (l *Listener) RunHBListener() {
 			receivedMsg := msg.JSONToMsg([]byte(string(hbBuf[:n])))
 			msg.PrintMsg(receivedMsg)
 		}
-		
+
 		if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
 			//Timeout error
 			fmt.Printf("HBListener: Client %s Timeout!\n", msgAddr)
@@ -119,4 +117,3 @@ func (l *Listener) RunHBListener() {
 		}
 	}
 }
-
