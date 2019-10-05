4c2a3a0d96edf3ae6066a0d944de8790f91da747
Xiang Li <lidiang2507@gmail.com>
2019-10-05 12:00:56 -0500

    node/Listener.go

--- src/MP2/src/Node/Node.go
+++ src/MP2/src/Node/Node.go
@@ -58,6 +58,7 @@ func RunNode(isIntroducer bool) {
 //Called from main.go when the command is "LEAVE\n"
 //Delete the Node
 func StopNode() {
+	fmt.Println("Node: Stop Node...")
 	curNode.Sender.NodeSend(msg.LeaveMsg)
 	Status = false
 	KillRoutine <- struct{}{}
