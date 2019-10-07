aae2fa27536236c1441df73ae3da274dcbc9b403
Xiang Li <lidiang2507@gmail.com>
2019-10-07 11:30:59 -0500

    V5.1

--- src/MP2_finished/src/Node/Sender.go
+++ src/MP2_finished/src/Node/Sender.go
@@ -67,7 +67,7 @@ func (s *Sender) SendHeartbeat() {
 			randNum := rand.Int63n(max-min) + min
 			cnt += 1
 			log.Printf("Generate Heartbeat %d round..\n", cnt)
-			if randNum >= test2 {
+			if randNum >= test1 {
 				for _, monitorID := range MonitorList {
 					monitorAddress := msg.GetIPAddressFromID(monitorID)
 					udpAddr, err := net.ResolveUDPAddr(msg.ConnType, monitorAddress+":"+msg.HeartbeatPort)
