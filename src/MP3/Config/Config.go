ff7b2089c1cfa400fb0d1d5402bc8e85bc2524d7
Liu <taipeng2@fa19-cs425-g73-01.cs.illinois.edu>
2019-10-18 15:46:22 -0500

    MP3 basic

---
+++ src/MP3/Config/Config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	ConnHostName      = "fa19-cs425-g73-%02d.cs.illinois.edu"
+	ConnType          = "udp"
+	ConnPort          = "8888"
+	HeartbeatPort     = "8887"
+	IntroducePort     = "8886"
+	ConnlocalHost     = "localhost"
+	TimeOut           = 4100
+	IntroducerAddress = "fa19-cs425-g73-01.cs.illinois.edu"
+)
+
+func CreateID() string {
+	hostName := GetHostName()
+	localTime := time.Now()
+	return hostName + ":" + localTime.Format("20060102150405")
+}
+
+func GetIPAddressFromID(ID string) string {
+	return strings.Split(ID, ":")[0]
+}
+
+func GetHostName() string {
+	cmd := exec.Command("hostname")
+	hName, _ := cmd.Output()
+	oldName := string(hName)
+	hostName := strings.Replace(oldName, "\n", "", -1)
+	return hostName
+}
+
+func GetVMNumber() int {
+	cmd := exec.Command("hostname")
+	hName, _ := cmd.Output()
+	hostName := string(hName)
+	var machineNO int
+	if hostName[15] == '0' {
+		machineNO, _ = strconv.Atoi(hostName[16:17])
+	} else {
+		machineNO, _ = strconv.Atoi(hostName[15:17])
+	}
+	return machineNO
+}
+
+func IsIntroducer() bool {
+	hostName := GetHostName()
+	return hostName == IntroducerAddress
+}
---
+++ src/MP3/MsgProtocol/Message.go
@@ -0,0 +1,47 @@
+package MsgProtocol
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const (
+	HeartbeatMsg = "Heartbeat"
+	JoinMsg      = "Join" //Content is empty
+	LeaveMsg     = "Leave"
+	FailMsg      = "Fail"
+	IntroduceMsg = "Introduce" // Content will include new-join node's ID
+	JoinAckMsg   = "JoinAck"   // Content will include full membership list
+)
+
+type Message struct {
+	MessageType string   //Heartbeat, Join, Leave, Introduce,(IntroduceAck?)
+	NodeID      string   //Local ID
+	Content     []string //Message Content
+}
+
+func NewMessage(Type string, ID string, Content []string) Message {
+	newMessage := Message{
+		MessageType: Type,
+		NodeID:      ID,
+		Content:     Content,
+	}
+	return newMessage
+}
+
+func MsgToJSON(message Message) []byte {
+	b, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return b
+}
+
+func JSONToMsg(b []byte) Message {
+	var m Message
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return m
+}
