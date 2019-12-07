b946e613c65142e89e7fcac3332a1f9979393d5c
Xiang Li <lidiang2507@gmail.com>
2019-12-06 21:39:23 -0600

    Reduce

--- src/MP4/Reducer/WordCountReducer.go
+++ src/MP4/Reducer/WordCountReducer.go
@@ -5,41 +5,40 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
+	// "strings"
 )
 
 //General format as: key : [val]\n
 
-func wordCount(content string) int{
-	var cnt = 0 
+func wordCount(content string) int {
+	var cnt = 0
 	var startIdx = 0
 	//Find value (Todo: make sure [ will not appear at end of line)
 	for idx, c := range content {
 		if c == '[' {
 			startIdx = idx + 1
 		} else if c == ']' {
-			val = content[startIdx:idx]
+			val := content[startIdx:idx]
 			cnt += val
 		}
 	}
 	return cnt
 }
 
-func postProcess() string {
-	res := ""
+// func postProcess() string {
+// 	res := ""
 
-	for idx, val := range valueList {
-		if idx != len(valueList) - 1 {
-			res += val + ","
-		} else {
-			res += val + "\n"
-		}
-	}
-	
+// 	for idx, val := range valueList {
+// 		if idx != len(valueList) - 1 {
+// 			res += val + ","
+// 		} else {
+// 			res += val + "\n"
+// 		}
+// 	}
 
-	fmt.Println(res)
-	return res
-}
+// 	fmt.Println(res)
+// 	return res
+// }
 
 func main() {
 	var totalCnt = 0
@@ -69,5 +68,5 @@ func main() {
 
 	// res := PostProcess(output)
 
-	fmt.Fprintf(os.Stdout, totalCnt)
+	fmt.Fprintf(os.Stdout, strconv.Itoa(totalCnt))
 }
